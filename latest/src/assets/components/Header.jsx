365a5bb44583479b0ce8336c05e3f49b9cacc04d
Araujo Manon <araujo.manon@laposte.net>
2024-05-17 15:22:02 +0200

    Création de la navigation

---
+++ latest/src/assets/components/Header.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+import logoKaza from "../images/LOGO.png";
+const Header = () => {
+  return (
+    <div className="header">
+    <img className="logoKaza" src={logoKaza} alt="Logo Kasa" />
+    <nav className="nav">
+      <NavLink to="/" className={({ isActive }) => (isActive ? "underline" : "")}>
+        <p>Accueil</p>
+      </NavLink>
+      <NavLink to="/About"className={({ isActive }) => (isActive ? "underline" : "")}>
+        <p>A Propos</p>
+      </NavLink>
+    </nav>
+  </div>
+  );
+};
+export default Header;
---
+++ latest/src/assets/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+    return (
+    <div className="home">
+      <div className="banner">
+     <img className="bannerImg" src={banner} alt="Image de montagne"/>
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
+
+      </div>
+    );
+  };
+  
+  export default Home;
